91f59d562ed3ed6e745d4d67adfa74f13aa54810
Bruno Torres <bruno.torres.gomez@gmail.com>
2018-12-14 14:00:07 -0800

    add redux middleware to debug state

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,12 +1,13 @@
-import  { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "../reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant()) //-- we actually invoke reduxImmutableStateInvariant 
-    );
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk, reduxImmutableStateInvariant())) //-- we actually invoke reduxImmutableStateInvariant
+  );
 }
